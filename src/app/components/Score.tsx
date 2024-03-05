a69a4db1be86f29524f3302d2d49715b6e09792d
danish sheikh <130551259+danish3211@users.noreply.github.com>
2024-03-05 16:35:25 +0530

    wide ball

--- src/app/components/Score.tsx
+++ src/app/components/Score.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { Input, Select, SelectItem } from "@nextui-org/react";
+"use client";
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
 import React, { useState } from "react";
 
 interface OverDetails {
@@ -31,7 +31,10 @@ const Score = () => {
     if (action === "wicket") {
       setWickets(wickets + 1);
       addBall("Wicket");
-    } else {
+    }  else if (action === "wide") {
+      setRuns(runs + 1); // Wide counts as 1 run.
+      addBall("Wide");
+    }else {
       const runValue = parseInt(action, 10);
       if (!isNaN(runValue)) {
         setRuns(runs + runValue);
@@ -41,11 +44,20 @@ const Score = () => {
   };
 
   const addBall = (result: string) => {
-    const newBall = `${overs}.${ballsInOver.length + 1}: ${result}`;
-    setBallsInOver([...ballsInOver, newBall]);
-    setBalls(balls + 1);
-
-    if (balls === 5) {
+    if (result === "Wide") {
+      const newBall = `${overs}.${ballsInOver.length + 1}: ${result}`;
+      setBallsInOver([...ballsInOver, newBall]);
+      // Don't increment the balls count for wide balls
+      return;
+    }
+  
+    if (balls < 6) {
+      const newBall = `${overs}.${ballsInOver.length + 1}: ${result}`;
+      setBallsInOver([...ballsInOver, newBall]);
+      setBalls(balls + 1);
+    }
+  
+    if (balls === 6) {
       const overDetails: OverDetails = {
         over: overs,
         runs: runs,
@@ -53,9 +65,10 @@ const Score = () => {
         balls: ballsInOver,
         bowler: bowlerName,
       };
-
+  
       setOverHistory([...overHistory, overDetails]);
-
+  
+      // Reset balls and ballsInOver only if it's not a wide ball
       setBalls(0);
       setBallsInOver([]);
       setOvers(overs + 1);
@@ -63,77 +76,139 @@ const Score = () => {
       setWickets(0);
     }
   };
+  
+  
+
+  const handleDeleteBall = (index: number) => {
+    const updatedBallsInOver = ballsInOver.slice();
+    updatedBallsInOver.splice(index, 1);
+    setBallsInOver(updatedBallsInOver);
+    setBalls(balls - 1);
+  };
+
   return (
     <div className="flex flex-col items-center min-h-screen bg-gradient-to-r from-[#090979] to-black text-white p-8">
-    <p className="text-3xl font-bold  text-red-500">Score Counter</p>
-    <p className="text-sm font-medium mb-4 text-gray-300">by @Danish Shaikh</p>
-    <div className="mb-4  text-center">
-      <p className="text-xl">Runs: {runs}</p>
-      <p className="text-xl">Wickets: {wickets}</p>
-      <p className="text-xl">Total Overs: {overs}</p>
-    </div>
+      <p className="text-3xl font-bold  text-red-500">Score Counter</p>
+      <p className="text-sm font-medium mb-4 text-gray-300">
+        by @Danish Shaikh
+      </p>
+      <div className="mb-4  text-center">
+        <p className="text-[22px] font-medium">
+          Total Runs: <span className="text-green-500"> {runs}</span>
+        </p>
+        <p className="text-[22px] font-medium">
+          Total Wickets: <span className="text-red-500"> {wickets}</span>
+        </p>
+        <p className="text-[22px] font-medium">Total Overs: {overs}</p>
+      </div>
 
-    <div className="mb-4 w-[200px]">
-      <Select
-        color="warning"
-        label="Add Runs or Wickets"
-        onChange={handleAdd}
-      >
-        <SelectItem key="0" value="0">
-          0
-        </SelectItem>
-        <SelectItem key="1" value="1">
-          Single
-        </SelectItem>
-        <SelectItem key="2" value="2">
-          Double
-        </SelectItem>
-        <SelectItem key="4" value="4">
-          Four
-        </SelectItem>
-        <SelectItem key="6" value="6">
-          Sixer
-        </SelectItem>
-        <SelectItem key="wicket" value="wicket">
-          Wicket
-        </SelectItem>
-      </Select>
-      <Input
-        color="success"
-        className="mt-2 hover:shadow-lg transition duration-300"
-        type="text"
-        value={bowlerName}
-        onChange={(e) => setBowlerName(e.target.value)}
-        placeholder="Enter Bowler Name"
-      />
-    </div>
+      <div className="mb-4 w-[200px]">
+        <Select
+          color="warning"
+          label="Add Runs or Wickets"
+          onChange={handleAdd}
+        >
+          <SelectItem key="0" value="0">
+            0
+          </SelectItem>
+          <SelectItem key="1" value="1">
+            Single
+          </SelectItem>
+          <SelectItem key="2" value="2">
+            Double
+          </SelectItem>
+          <SelectItem key="4" value="4">
+            Four
+          </SelectItem>
+          <SelectItem key="6" value="6">
+            Sixer
+          </SelectItem>
+          <SelectItem key="wide" value="wide" style={{ color: "#0067B3" }}>
+            Wide
+          </SelectItem>
+          <SelectItem key="wicket" value="wicket" style={{ color: "red" }}>
+            Wicket
+          </SelectItem>
+        </Select>
+        <Input
+          color="success"
+          className="mt-2 hover:shadow-lg transition duration-300"
+          type="text"
+          value={bowlerName}
+          onChange={(e) => setBowlerName(e.target.value)}
+          placeholder="Enter Bowler Name"
+        />
+      </div>
 
-    <div className="mt-4">
-      <p className="text-xl font-bold">Balls in Over:</p>
-      <ul className="list-disc pl-4">
-        {ballsInOver.map((ball, index) => (
-          <li key={index}>{ball}</li>
-        ))}
-      </ul>
-    </div>
+      <div className="mt-4">
+        <p className="text-xl font-bold text-center">Balls in Over:</p>
+        <ul className="list-disc pl-4">
+          {ballsInOver.map((ball, index) => (
+            <li
+              key={index}
+              className="flex justify-between items-center gap-2 space-y-2"
+            >
+              <span
+                 className={
+                  ball.includes("Wide")
+                    ? "text-yellow-500"
+                    : ball.includes("Wicket")
+                    ? "text-red-500"
+                    : ""
+                }
+              >
+                {ball}
+              </span>
+              <Button
+                size="sm"
+                className="text-red-500 font-medium hover:text-red-700 bg-red-200 cursor-pointer"
+                onClick={() => handleDeleteBall(index)}
+              >
+                Delete
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-    <div className="mt-4">
-      <p className="text-xl font-bold">Over History:</p>
-      <ul>
-        {overHistory.map((over, index) => (
-          <li key={index} className=" p-4 rounded-lg transition duration-300">
-            Over {over.over} - Runs: {over.runs}, Wickets: {over.wickets},{" "}
-            Bowler: {over.bowler}
-            <ul>
-              {over.balls.map((ball, index) => (
-                <li key={index}>{ball}</li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      <div className="mt-4">
+        <p className="text-xl font-bold text-center">Over History:</p>
+        <ul>
+          {overHistory.map((over, index) => (
+            <li key={index} className=" p-2 rounded-lg transition duration-300">
+              <p className="text-[22px] font-medium">
+                {over.over} over by {over.bowler}
+              </p>
+              <ul className="text-center">
+                {over.balls.map((ball, index) => (
+                  <li key={index}>
+                    <span
+                       className={
+                        ball.includes("Wide")
+                          ? "text-yellow-500"
+                          : ball.includes("Wicket")
+                          ? "text-red-500"
+                          : ""
+                      }
+                    >
+                      {ball}
+                    </span>
+                  </li>
+                ))}
+                <p className="text-[22px] font-medium">
+                  Total Runs:{" "}
+                  <span className="text-green-500"> {over.runs}</span>
+                </p>
+                <p className="text-[22px] font-medium">
+                  Total Wickets:{" "}
+                  <span className="text-red-500"> {over.wickets}</span>
+                </p>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
   );
 };
 
